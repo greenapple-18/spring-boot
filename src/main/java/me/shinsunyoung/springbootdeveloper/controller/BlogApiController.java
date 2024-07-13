8f38f5453570c0cfa113594053e2d3f70fe7fb6d
greenapple <dla16253745@gmail.com>
2024-07-13 14:36:26 +0900

    [Update]: complete blog api

--- src/main/java/me/shinsunyoung/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/shinsunyoung/springbootdeveloper/controller/BlogApiController.java
@@ -57,6 +57,9 @@ public class BlogApiController {
     @PutMapping("/api/articles/{id}")
     public ResponseEntity<Article> updateArticle(@PathVariable long id,
                                                  @RequestBody UpdateArticleRequest request){
+        Article updatedArticle = blogService.update(id, request);
 
+        return ResponseEntity.ok()
+                .body(updatedArticle);
     }
 }
