aada62d88eddee1625e54b7f6c6a2a915dec9c34
greenapple <dla16253745@gmail.com>
2024-07-18 16:58:38 +0900

    [Update]:Add domain, token filter, API

--- src/test/java/me/shinsunyoung/springbootdeveloper/config/jwt/JwtFactory.java
+++ src/test/java/me/shinsunyoung/springbootdeveloper/config/jwt/JwtFactory.java
@@ -1,11 +1,10 @@
-package me.shinsunyoung.springbootdeveloper.controller.config.jwt;
+package me.shinsunyoung.springbootdeveloper.config.jwt;
 
 import io.jsonwebtoken.Header;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.Builder;
 import lombok.Getter;
-import me.shinsunyoung.springbootdeveloper.config.jwt.JwtProperties;
 
 import java.time.Duration;
 import java.util.Date;
